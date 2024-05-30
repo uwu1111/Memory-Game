e6e77313acc84b195df28bc62e6dfe375d21dfd9
Fozik <fozikwashere@mail.ru>
2024-05-30 20:33:58 +0500

    Created File

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "Memory.h"
+
+using namespace std;
+
+int main()
+{
+	int gameindx;
+	cout << "Welcome To Memory Game!" << endl;
+	cout << "Which Difficult Do You Prefer?" << endl << "1. 4x4" << endl << "2. 8x8" << endl << "3. 16x16" << endl << "4. 32x32" << endl;
+	cin >> gameindx;
+	MemoryCreator(gameindx);
+	char** arr = new char* [4]; // строки
+	for (int i = 0; i < 4; i++)
+	{
+		arr[i] = new char[4]; // столбцы
+	}
+}
---
+++ Memory.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include <stdlib.h>
+#include <windows.h>
+
+using namespace std;
+
+char symbols[18]{'!','%','?','\\','/','+','*','$','@','#','&','№','<','>',':',';','-','_'};
+char** arr; // строки
+
+void MemoryCreator(int index)
+{
+	if (index % 2 == 0)
+	{
+		int counter = 0;
+		srand(time(0));
+		arr = new char* [index];
+		for (int i = 0; i < index; i++)
+		{
+			arr[i] = new char[index]; // столбцы
+		}
+		for (int i = 0; i < index; i++)
+		{
+			for (int j = 0; j < index - 1; j+=2)
+			{
+				int randnum = rand() % 10;
+				arr[i][j] = symbols[counter];
+				arr[i][j + 1] = symbols[counter];
+				counter++;
+				//cout << arr[i][j] << " ";
+			}
+			//cout << endl;
+		};
+		for (int i = 0; i < index; i++)
+		{
+			for (int j = 0; j < index; j++)
+			{
+				cout << arr[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+	else
+	{
+		cout << "Error! Number Cant Be Divined By 2!";
+	}
+}
