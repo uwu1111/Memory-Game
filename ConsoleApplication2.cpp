ef22b17fa7ebe287c80fc039d68f651965c068ec
Fozik <fozikwashere@mail.ru>
2024-06-11 00:30:29 +0500

    Finally Works

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -1,18 +1,37 @@
 ﻿#include <iostream>
-#include "Memory.h"
+#include <chrono> 
+#include <thread>
+#include "library.h"
 
 using namespace std;
+using namespace std::chrono;
 
 int main()
 {
-	int gameindx;
-	/*cout << "Welcome To Memory Game!" << endl;
-	cout << "Which Difficult Do You Prefer?" << endl << "1. 4x4" << endl << "2. 8x8" << endl << "3. 16x16" << endl << "4. 32x32" << endl;*/
-	cin >> gameindx;
-	MemoryCreator(gameindx);
-	char** arr = new char* [4]; // строки
-	for (int i = 0; i < 4; i++)
+	int gameindex;
+	int choice;
+	cout << "Welcome To Memory Game!" << endl;
+	cout << "Which Difficult Do You Prefer?" << endl << "1. 4x4" << endl << "2. 6x6" << endl;
+	cin >> choice;
+	switch (choice)
 	{
-		arr[i] = new char[4]; // столбцы
+	case 1:
+		gameindex = 4;
+		break;
+	case 2:
+		gameindex = 6;
+		break;
 	}
+	MemoryCreator(gameindex);
+	char** arr = new char* [gameindex]; // строки
+	for (int i = 0; i < gameindex; i++)
+	{
+		arr[i] = new char[gameindex]; // столбцы
+	}
+	HideCard(gameindex);
+	auto begin = steady_clock::now();
+	Logic(gameindex);
+	auto end = steady_clock::now();
+	float time_seconds = duration_cast<seconds>(end - begin).count();
+	cout << "Total Time Spent On Game " << time_seconds << " seconds\n";
 }
---
+++ library.h
@@ -0,0 +1,208 @@
+﻿#pragma once
+#include <stdlib.h>
+#include <windows.h>
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+using namespace std::chrono;
+
+char symbols[18]{'!','%','?','\\','/','+','*','$','@','#','&',')','<','>',':',';','-','_'};
+int counter = 0;
+int flips = 0;
+char** arr; // строки
+bool** arr1;
+
+void PrintArr(int index)
+{
+	for (int i = 0; i < index; i++)
+	{
+		for (int j = 0; j < index; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void SwapSing(char& index, char& indextochange)
+{
+	char buffer_index = index;
+	index = indextochange;
+	indextochange = buffer_index;
+}
+
+void MemoryCreator(int index)
+{
+	if (index % 2 == 0)
+	{
+		int counter = 0;
+		srand(time(0));
+		arr = new char* [index];
+		for (int i = 0; i < index; i++)
+		{
+			arr[i] = new char[index]; // столбцы
+		}
+		for (int i = 0; i < index; i++)
+		{
+			for (int j = 0; j < index - 1; j+=2)
+			{
+				int random = rand() % 10;
+				arr[i][j] = symbols[counter];
+				arr[i][j + 1] = symbols[counter];
+				counter++;
+				//cout << arr[i][j] << " ";
+			}
+			//cout << endl;
+		};
+		int card1row, card1col;
+		int card2row, card2col;
+		switch (index)
+		{
+		case 4:
+			for (int i = 0; i < 25; i++)
+			{
+				card1col = rand() % 4;
+				card1row = rand() % 4;
+				card2col = rand() % 4;
+				card2row = rand() % 4;
+				int rand_num = rand() % 2;
+				system("cls");
+				SwapSing(arr[card1row][card1col], arr[card2row][card2col]);
+				PrintArr(index);
+			}
+			break;
+		case 6:
+			for (int i = 0; i < 25; i++)
+			{
+				card1col = rand() % 6;
+				card1row = rand() % 6;
+				card2col = rand() % 6;
+				card2row = rand() % 6;
+				int rand_num = rand() % 2;
+				system("cls");
+				SwapSing(arr[card1row][card1col], arr[card2row][card2col]);
+				PrintArr(index);
+			}
+			break;
+		}
+		
+	}
+	else
+	{
+		cout << "Error! Number Cant Be Divined By 2!";
+	}
+}
+void UpCard(int index)
+{
+	system("cls");
+	for (int i = 0;i < index;i++)
+	{
+		for (int j = 0; j < index; j++)
+		{
+			if (arr[i][j] == ' ')
+			{
+				cout << ' ';
+			}
+			else if (arr1[i][j] == false)
+			{
+				cout << "0";
+			}
+			else if (arr1[i][j] == true)
+			{
+				cout << arr[i][j];
+			}
+		}
+		cout << endl;
+	}
+}
+void HideCard(int index)
+{
+	arr1 = new bool* [index];
+	for (int i = 0; i < index; i++)
+	{
+		arr1[i] = new bool[index]; // столбцы
+	}
+	for (int i = 0; i < index;i++)
+	{
+		for (int j = 0;j < index;j++)
+		{
+			arr1[i][j] = false;
+		}
+	}
+	UpCard(index);
+}
+
+void Logic(int index)
+{
+	int stroke = -1;
+	int collum = -1;
+	int prev_nums[2];
+	for (;;)
+	{
+		if (stroke != -1 && collum != -1)
+		{
+			flips++;
+			cout << "Type Stroke: ";
+			cin >> stroke;
+			cout << "Type Collum: ";
+			cin >> collum;
+			arr1[stroke - 1][collum - 1] = true;
+			UpCard(index);
+			if (stroke - 1 == prev_nums[0] && collum - 1 == prev_nums[1])
+			{
+				cout << "Invalid, Syntax!\n";
+				continue;
+			}
+			if (stroke != prev_nums[0] || collum != prev_nums[1])
+			{
+				if (arr[stroke - 1][collum - 1] == arr[prev_nums[0]][prev_nums[1]])
+				{
+					if (counter == index * index / 2 - 1)
+					{
+						system("cls");
+						cout << "You Won!\n";
+						cout << "Total Card Flips " << flips << "\n";
+						return;
+					}
+					else
+					{
+						arr[stroke - 1][collum - 1] = ' ';
+						arr[prev_nums[0]][prev_nums[1]] = ' ';
+						counter++;
+						UpCard(index);
+					}
+				}
+				else
+				{
+					Sleep(400);
+					arr1[stroke - 1][collum - 1] = false;
+					arr1[prev_nums[0]][prev_nums[1]] = false;
+					UpCard(index);
+				}
+			}
+			else
+			{
+				Sleep(400);
+				arr1[stroke - 1][collum - 1] = false;
+				arr1[prev_nums[0]][prev_nums[1]] = false;
+				UpCard(index);
+			}
+			stroke = -1;
+			collum = -1;
+		}
+		else if (stroke == -1 && collum == -1)
+		{
+			flips++;
+			cout << "Type Stroke: ";
+			cin >> stroke;
+			cout << "Type Collum: ";
+			cin >> collum;
+			prev_nums[0] = stroke - 1;
+			prev_nums[1] = collum - 1;
+			arr1[stroke - 1][collum - 1] = true;
+			UpCard(index);
+		}
+	}
+}
